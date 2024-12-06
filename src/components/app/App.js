ae550d19b46b392c3bdd562bb2d5cc4476e28c52
x404 <kronos2003@gmail.com>
2024-12-06 18:08:01 +0200

    feat: added Router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,23 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
-
-import {useState} from "react";
-import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
+import {MainPage, ComicsPage} from "../pages";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route exact path="/" element={<MainPage/>}></Route>
+                        <Route exact path="/comics" element={<ComicsPage/>}></Route>
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
-export default App; 
+export default App;
