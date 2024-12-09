00ac69542d3abf347452834a6d54b010c9596669
x404 <kronos2003@gmail.com>
2024-12-09 16:53:28 +0200

    fix: fixed NavLink syntax

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,6 +1,5 @@
-
 import './appHeader.scss';
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const AppHeader = () => {
     return (
@@ -12,9 +11,15 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><NavLink exact active={{'color': '#9f0013'}} to="/">Characters</NavLink></li>
+                    <li><NavLink
+                        end
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                        to="/">Characters</NavLink></li>
                     /
-                    <li><NavLink exact active={{'color': '#9f0013'}} to="/comics">Comics</NavLink></li>
+                    <li><NavLink
+                        end
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                        to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
